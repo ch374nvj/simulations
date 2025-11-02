cf4f27a53f9d9c508b8e7b40247061b5101a959a
Chetan VJ <98468274+ch374nvj@users.noreply.github.com>
2025-11-02 17:13:49 +0530

    Add constants, model pdf

--- Control System/Phase Locked Loop/startup.m
+++ Control System/Phase Locked Loop/startup.m
@@ -13,7 +13,12 @@ Ki = 1e+4;
 fc = 300;
 Wc = 2*pi*fc;
 tau = 1/Wc;
-%%
+%% 
 t1 = tf([Kp Ki],[1 Wc 0])
 bp = bodeplot(t1)
 bp.FrequencyUnit = "Hz";
+
+%% PFD
+Icp = 50e-6; %uA
+Tup = 100e-9; %ns
+C = 1e-6; %uF
